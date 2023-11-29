b5f1728f68b7db210fabcecd87919239f1367c6f
root <root@AlaaSayed.localdomain>
2023-11-29 16:20:24 +0200

    task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ''
+    for ch in str:
+        if 'a' <= ch <= 'z':
+            uppercase_ch = chr(ord(ch) - 32)
+            new_str += uppercase_ch
+        else:
+            new_str += ch
+    print(new_str)
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
