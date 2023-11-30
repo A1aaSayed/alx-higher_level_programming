a2239b0a93f8ce329e8a3c9774c407ab2b6b0ba3
root <root@AlaaSayed.localdomain>
2023-12-01 00:31:36 +0200

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-args = sys.argv
-args_len = len(sys.argv) - 1
+if __name__ == '__main__':
+    import sys
+    args = sys.argv
+    args_len = len(sys.argv) - 1
 
-if args_len == 0:
-    print('{} arguments.'.format(0))
-elif args_len == 1:
-    print('{} argument:'.format(1))
-    print('{}: {:s}'.format(1, args[1]))
-else:
-    print('{} arguments:'.format(args_len))
-    for i in range(1, args_len + 1):
-        print('{}: {:s}'.format(i, args[i]))
+    if args_len == 0:
+        print('{} arguments.'.format(0))
+    elif args_len == 1:
+        print('{} argument:'.format(1))
+        print('{}: {:s}'.format(1, args[1]))
+    else:
+        print('{} arguments:'.format(args_len))
+        for i in range(1, args_len + 1):
+            print('{}: {:s}'.format(i, args[i]))
