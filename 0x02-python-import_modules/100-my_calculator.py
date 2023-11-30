8af171036368305b97adf97bc32f1b5aaa5ec8dc
root <root@AlaaSayed.localdomain>
2023-12-01 00:58:41 +0200

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,19 +8,18 @@ if __name__ == '__main__':
     if args_len != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    elif args[2] not in ['+', '-', '*', '/']:
+    if args[2] == '+':
+        print('{} + {} = {}'
+                .format(args[1], args[3], add(args[1], args[3])))
+    elif args[2] == '-':
+        print('{} - {} = {}'
+                .format(args[1], args[3], sub(args[1], args[3])))
+    elif args[2] == '*':
+        print('{} * {} = {}'
+                .format(args[1], args[3], mul(args[1], args[3])))
+    elif args[2] == '/':
+        print('{} / {} = {}'
+                .format(args[1], args[3], div(args[1], args[3])))
+    else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
-    else:
-        if args[2] == '+':
-            print('{} + {} = {}'
-                  .format(args[1], args[3], add(args[1], args[3])))
-        elif args[2] == '-':
-            print('{} - {} = {}'
-                  .format(args[1], args[3], sub(args[1], args[3])))
-        elif args[2] == '*':
-            print('{} * {} = {}'
-                  .format(args[1], args[3], mul(args[1], args[3])))
-        elif args[2] == '/':
-            print('{} / {} = {}'
-                  .format(args[1], args[3], div(args[1], args[3])))
