a5058f2774018d8e0744f0321fa9d0bea0c8f668
root <root@AlaaSayed.localdomain>
2023-12-07 01:55:30 +0200

    up

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,11 @@
+/**
+ * print_python_list - print some basic info about
+ * Python lists and Python bytes objects
+ * @p: pointer
+ * Return: void
+*/
+
+void print_python_list(PyObject *p)
+{
+
+}
