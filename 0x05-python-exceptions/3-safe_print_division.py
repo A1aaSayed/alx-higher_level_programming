dfa04587bad771153775e354b86a64c6e96393b0
root <root@AlaaSayed.localdomain>
2023-12-20 22:07:39 +0200

    up

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-	try:
-		result = a / b
-	except ZeroDivisionError:
-		result = None
-	finally:
-		print("Inside result: {}".format(result))
-	return result
+    try:
+        division = a / b
+    except (TypeError, ZeroDivisionError):
+        division = None
+    finally:
+        print('Inside result: {}'.format(division))
+    return division
