439156374b2bc045833daa9c64eafc2f5859408e
root <root@AlaaSayed.localdomain>
2023-12-23 16:45:39 +0200

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """Square"""
+    def __init__(self, size):
+        """
+        Initialize the square with a given size
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
