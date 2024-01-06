c413492136c638c3a4ae5200456b37f0c6c0434d
root <root@AlaaSayed.localdomain>
2024-01-06 15:49:20 +0200

    up

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -76,5 +76,5 @@ class Rectangle:
 
     def __str__(self):
         """prints in stdout the square"""
-        return '\n'.join(['#' * self.__width for _ in range(self.__height)] 
+        return '\n'.join(['#' * self.__width] * self.__height 
                          if self.__width != 0 and self.__height != 0 else '')
