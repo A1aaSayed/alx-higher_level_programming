72bef51e314ed811e0809e760106b975ad4fbfae
root <root@AlaaSayed.localdomain>
2024-01-20 13:43:15 +0200

    up

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Rectangle Class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle"""
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
