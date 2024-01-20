042b0187e12a25d456121e1e946e5e17fc629dc3
root <root@AlaaSayed.localdomain>
2024-01-20 13:06:01 +0200

    up

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""is_same class"""
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance
+    of the specified class, otherwise False"""
+    if not isinstance(obj, a_class):
+        return False
+    else:
+        return True
