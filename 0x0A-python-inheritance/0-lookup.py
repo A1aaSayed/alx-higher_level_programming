4c28a2eea568601a2ef5e83b82fc2a97682d03b9
root <root@AlaaSayed.localdomain>
2024-01-20 12:09:18 +0200

    up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+""" lookup function """
+
+
 def lookup(obj):
+    """
+    Function that returns a list containing the available
+    attributes and methods of an object.
+    """
     return [attribute for attribute in dir(obj)]
