6bf9c5d6261fb7c9fbb9e4937f5a4284c919d3ae
root <root@AlaaSayed.localdomain>
2024-01-20 12:14:55 +0200

    up

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""MyList Class"""
+
+
+class MyList(list):
+    """Sub class from list"""
+    def print_sorted(self):
+        """Function to print list"""
+        print(sorted(self))
