5ce1cb1b51dda98d6d8c392f99d96dbe88211944
root <root@AlaaSayed.localdomain>
2024-01-22 12:18:18 +0200

    up

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Divide all elements of a matrix."""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divide all elements of a matrix.
+
+    Args:
+        matrix (array): array of elements
+        div (int or float): An integer or float
+
+    Return:
+        The divition
+    """
