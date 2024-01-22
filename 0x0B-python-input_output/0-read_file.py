74829606fe45edbeb539a03d9e620aed72eb77d3
root <root@AlaaSayed.localdomain>
2024-01-22 15:03:53 +0200

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """function that reads a text file (UTF8) and prints it to stdout"""
 
+
 def read_file(filename=""):
     """read file"""
-    with open(filename, mode='r', encoding='UTF-8') as f:
+    with open(filename, 'r', encoding='UTF-8') as f:
         print(f.read(), end='')
