7dd1cd086cb74ad85ae5df0dbd30fb9a5d059b4d
A1aaSayed <alaa02.sayed02@gmail.com>
2024-01-24 14:45:22 +0200

    up

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,6 +7,7 @@ class MyInt(int):
     def __eq__(self, other):
         """equal method"""
         return super().__ne__(other)
+
     def __ne__(self, other):
         """negative method"""
         return super().__eq__(other)
