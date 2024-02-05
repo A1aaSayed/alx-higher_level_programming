11fe87279bf2885bc835fcd0d3f12747053d900a
A1aaSayed <alaa02.sayed02@gmail.com>
2024-02-05 02:07:08 +0200

    up

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,14 +6,14 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Square Class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
         """get the size of the square"""
-        return self.__width
+        return self.width
     
     @size.setter
     def size(self, value):
