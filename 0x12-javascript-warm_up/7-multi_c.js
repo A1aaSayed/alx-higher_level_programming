b2fd564e5102b6f795d9506e68267aefc2c2d8fe
A1aaSayed <alaa02.sayed02@gmail.com>
2024-03-14 04:25:17 +0200

    up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,7 +7,7 @@ const n = parseInt(argv[2]);
 if (isNaN(n) || argv.length === 2) {
   console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < n; i++) {
-      console.log(str);
-    }
+  for (let i = 0; i < n; i++) {
+    console.log(str);
+  }
 }
