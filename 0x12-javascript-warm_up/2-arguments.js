5b628b2403d7421e94e69d6c721829f725dbf275
A1aaSayed <alaa02.sayed02@gmail.com>
2024-03-14 03:10:13 +0200

    up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,11 +3,11 @@
 const {argv} = require('process');
 
 if (argv.length === 2) {
-  console.log('No argument');
+    console.log('No argument');
 }
 else if (argv.length === 3) {
-  console.log('Argument found');
+    console.log('Argument found');
 }
 else {
-  console.log('Arguments found');
+    console.log('Arguments found');
 }
