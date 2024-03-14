46f020824ddb1880b428c922a809f827d41ae027
A1aaSayed <alaa02.sayed02@gmail.com>
2024-03-14 04:17:03 +0200

    up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const { argv } = require('process');
 const str = 'C is fun';
 const n = parseInt(argv[2]);
 
-if (isNaN(n)) {
+if (isNaN(n) || argv.length === 2) {
   console.log('Missing number of occurrences');
 } else {
     for (let i = 0; i < n; i++) {
