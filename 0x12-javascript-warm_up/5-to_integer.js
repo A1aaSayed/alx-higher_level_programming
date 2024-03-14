95b83a9112c6260237561c5ff93978508789ce22
A1aaSayed <alaa02.sayed02@gmail.com>
2024-03-14 03:32:45 +0200

    up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,10 +2,10 @@
 
 const { argv } = require('process');
 const str = argv[2];
-const num = Number(str);
+const num = parseInt(str);
 
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.profile('My number: ' + Math.floor(num));
+  console.log('My number: ' + Math.floor(num));
 }
