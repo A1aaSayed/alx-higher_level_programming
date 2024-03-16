cebb31cd8807e8db3d857c4459307f1ea1e19163
A1aaSayed <alaa02.sayed02@gmail.com>
2024-03-17 01:41:54 +0200

    up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function computeFactorial(num) {
+  if (num == 0 || isNaN(num))
+    return 1;
+  return num * computeFactorial(num - 1);
+}
+
+const input = process.argv[2];
+const number = parseInt(input);
+const factorial = computeFactorial(number);
+
+console.log(factorial);
