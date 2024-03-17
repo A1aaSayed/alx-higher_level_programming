cad0a78a20a58921806c96ff2a483a654bde9e58
A1aaSayed <alaa02.sayed02@gmail.com>
2024-03-17 04:32:57 +0200

    up

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,7 @@ class Square extends require('./5-square') {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 }
