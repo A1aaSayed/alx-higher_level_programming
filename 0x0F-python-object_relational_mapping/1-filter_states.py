6d624742cf253bbc4ae10d22c41b767183977d51
A1aaSayed <alaa02.sayed02@gmail.com>
2024-03-18 04:28:25 +0200

    up

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,9 +17,8 @@ if __name__ == '__main__':
 
     cursor = conn.cursor()
 
-    cursor.execute(
-        """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"""
-    )
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id')
+
 
     states = cursor.fetchall()
 
