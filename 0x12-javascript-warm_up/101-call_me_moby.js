8e24aa5b6a722bb0e87157c65679be1c60303b81
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-22 23:17:50 +0200

    javascript

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,6 +2,6 @@
 exports.callMeMoby = function callMeMoby (x, theFunction) {
     if (x > 0) {
       theFunction();
-      callMeMoby(x - 1, theFunction);
+      callMeMoby (x - 1, theFunction);
     }
 };
