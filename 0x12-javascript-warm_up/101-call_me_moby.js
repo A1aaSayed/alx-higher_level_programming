bc020d56785d604ba4271c659b78703be94a9842
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-22 23:18:19 +0200

    javascript

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.callMeMoby = function callMeMoby (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
     if (x > 0) {
       theFunction();
       callMeMoby (x - 1, theFunction);
