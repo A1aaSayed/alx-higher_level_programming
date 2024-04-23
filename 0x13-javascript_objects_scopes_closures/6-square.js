4c0157755f529033ec215d651831fee23a3327cd
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-23 19:43:13 +0200

    up

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+class Square extends require('./5-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+        if (c === undefined) {
+            console.log('X'.repeat(this.size));
+        } else {
+            console.log(c.repeat(this.size))
+        }
+    }
+  }
+}
+
+module.exports = Square;
