7e8fe069e4c252b6e4214b1e72c56876bf987274
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-23 19:31:11 +0200

    up

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,15 +14,15 @@ class Rectangle {
     }
   }
 
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
   rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
-  }
-  
-  double () {
-    this.width *= 2;
-    this.height *= 2;
   }
 }
 
