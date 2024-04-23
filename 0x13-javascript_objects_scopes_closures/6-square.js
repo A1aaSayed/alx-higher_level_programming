4faebf20b05d112c0a3f35459034eb4e1a31a703
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-23 19:45:58 +0200

    up

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 
 class Square extends require('./5-square') {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let i = 0; i < this.height; i++) {
-        if (c === undefined) {
-            console.log('X'.repeat(this.width));
-        } else {
-            console.log(c.repeat(this.width));
-        }
+      console.log(c.repeat(this.width));
     }
   }
 }
