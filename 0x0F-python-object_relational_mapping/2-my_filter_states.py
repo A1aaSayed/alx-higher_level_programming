0d8865b8a44f7b92973601727783ee769c12a09a
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-29 02:54:30 +0300

    up

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     cur = db.cursor()
-
+    
     cur.execute("SELECT * FROM states \
                 WHERE states.name LIKE BINARY '{}' \
                 ORDER BY states.id".format(sys.argv[4]))
