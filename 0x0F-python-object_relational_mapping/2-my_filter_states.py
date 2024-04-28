442c4fb9f4034fb0417a7b0c168cfcd6e34eba52
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-29 02:45:55 +0300

    up

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
-"""
-script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa
-"""
-
+"""script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
-
 import sys
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    try:
-        db = MySQLdb.connect(
-            host='localhost', port=3306,
-            user=username, passwd=password,
-            db=database
-        )
-    except MySQLdb.Error as e:
-        print(f'Error Connecting to Database {e}')
-        sys.exit(1)
-    
+    db = MySQLdb.connect(
+        host='localhost', user=username,
+        passwd=password, db=database, port=3306
+    )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(sys.argv[4]))
 
-    states = cur.fetchall()
+    query = """SELECT * FROM states
+                WHERE states.name LIKE BINARY '{}'
+                ORDER BY states.id
+            """.format(sys.argv[4])
+    cur.execute(query)
 
-    for state in states:
-        print(state)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
 
     cur.close()
     db.close()
