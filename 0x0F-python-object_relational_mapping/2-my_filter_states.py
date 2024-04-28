de7af5459648f5c0fc5788095039f12f5580fbf0
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-29 02:49:09 +0300

    up

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,9 +13,10 @@ if __name__ == '__main__':
     database = sys.argv[3]
 
     db = MySQLdb.connect(
-        host='localhost', user=username,
-        passwd=password, db=database, port=3306
-    )
+            host='localhost', port=3306,
+            user=username, passwd=password,
+            db=database
+        )
 
     cur = db.cursor()
 
