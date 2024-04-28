b9f532a8179399454e3564800350805d6d3ef3c5
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-29 02:47:02 +0300

    up

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument"""
+"""
+script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 import sys
