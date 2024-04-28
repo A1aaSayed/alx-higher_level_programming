5199300ae34ac05b2db7852d72e7e1a8fddda475
A1aaSayed <alaa02.sayed02@gmail.com>
2024-04-29 02:07:05 +0300

    up

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-def main():
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -20,6 +21,3 @@ def main():
 
     cur.close()
     db.close()
-
-if __name__ == '__main__':
-    main()
